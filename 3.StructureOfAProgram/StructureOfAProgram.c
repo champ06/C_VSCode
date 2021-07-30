ea5d79236a0c434f0e0cbead5543e3a77ee3016a
champ06 <rakuram.e96@gmail.com>
2021-07-30 10:45:09 +0530

    3.StructureOfAProgram

---
+++ 3.StructureOfAProgram/StructureOfAProgram.c
@@ -0,0 +1,21 @@
+// Understaning Structure of a Program
+
+// Preprocessor Directives
+#include <stdio.h>
+
+// Main Function with 2 paranthese signifies it is a function. Main Function is a Entry point to the program.
+// Everything inside the function will be executed sequentially.
+// Only one main function in a program.
+// Main function contents will be written within the curly braces.
+
+
+// C Program is CASE-SENSITIVE. Uppercase and lowercase are different.
+// Each line within the body will end with a semi-colon.
+// Indentation is used to indicate the block of code and make the code easily readable.
+
+int main()
+{
+    // Body of the function
+    printf("Hello world!!!");
+    return 0;
+}
