d03fdca9048447481d26a4bdcf5adfd42ece1d5f
champ06 <rakuram.e96@gmail.com>
2021-08-02 22:59:53 +0530

    Updated_Files

---
+++ Section 4 - Basic Concepts/27.Displaying_Output/020821_CLang_Displaying_Output.c
@@ -0,0 +1,14 @@
+/*
+* Author: Rakuram
+* Purpose: Displaying Output in C
+* Date: 02-08-2021
+*/
+
+#include<stdio.h>
+
+int main()
+{
+    printf("Hello World\n");
+    printf("This is Rakuram from India\n");
+    return 0;
+}
