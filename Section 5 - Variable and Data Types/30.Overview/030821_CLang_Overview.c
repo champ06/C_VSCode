39589941590c1ac3b4787dbf2f7cd8e23a453cd8
champ06 <rakuram.e96@gmail.com>
2021-08-03 23:14:38 +0530

    Lecture 29 - Overview

---
+++ Section 5 - Variable and Data Types/30.Overview/030821_CLang_Overview.c
@@ -0,0 +1,21 @@
+/*
+* Author: Rakuram
+* Purpose: Datatypes
+* Date: 03-08-2021
+*/
+
+#include<stdio.h>
+
+int main()
+{
+    // Declaring a variable
+    int i;
+
+    // Initializating a variable
+    int j = 0;
+
+    // Modifing a variable
+    j = 10;
+
+    return 0;
+}
