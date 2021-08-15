8cc00d228da2cfd77ac678adf1f4a00139e1e797
champ06 <rakuram.e96@gmail.com>
2021-08-15 18:05:18 +0530

    Cast and Sizeof Operators

--- Section 6 - Operators/41. Cast_Sizeof_Operators/150821_CLang_Cast_Sizeof_Operators.c
+++ Section 6 - Operators/41. Cast_Sizeof_Operators/150821_CLang_Cast_Sizeof_Operators.c
@@ -8,7 +8,13 @@
 
 int main()
 {
-    
+    int result;
+    float a = 25.6, b = 68.78;
+
+    result = (int)a + (int)b;
+    printf("With Casting, Result = %d\n\n", result);
+
+    printf("\'Sizeof\' Result = %d\n", sizeof(result));
 
     return 0;
 }
