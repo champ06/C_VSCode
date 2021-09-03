9fc932bcbada1771a1794cc85bf7342c732947aa
champ06 <rakuram.e96@gmail.com>
2021-09-03 23:51:28 +0530

    Task Code updated

--- Section 7 - Control Flow/55. Guess_the_number/310821_CLang_Guess_The_Number.c
+++ Section 7 - Control Flow/55. Guess_the_number/310821_CLang_Guess_The_Number.c
@@ -17,7 +17,30 @@ int main()
     printf("This is a GUESSING Game\n");
     printf("I have chosen a number between 0 and 20 which you have to guess\n");
 
-    printf("%d", randomNumber);
+    // printf("%d", randomNumber);
+
+    for (int i = 1; i <= 5; i++)
+    {
+        int EnteredNumber;
+        scanf ("%d", &EnteredNumber);
+        printf ("%d\n", EnteredNumber);
+
+        if (randomNumber > EnteredNumber)
+        {
+            printf("Entered number is too LOW\n");
+        }
+        
+        else if (randomNumber < EnteredNumber)
+        {
+            printf("Entered number is too HIGH\n");
+        }
+
+        else if (randomNumber == EnteredNumber)
+        {
+            printf("You entered correct number\n");
+            break;
+        }
+    }
 
     return 0;
 }
