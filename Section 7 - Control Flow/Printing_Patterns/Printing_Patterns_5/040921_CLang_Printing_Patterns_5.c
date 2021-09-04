b19748ee35d84dd3d1e0bef55776fad42e7df92f
champ06 <rakuram.e96@gmail.com>
2021-09-04 22:55:32 +0530

    Number Pattern Printing

---
+++ Section 7 - Control Flow/Printing_Patterns/Printing_Patterns_5/040921_CLang_Printing_Patterns_5.c
@@ -0,0 +1,34 @@
+/*
+** Printing Patterns 
+** Assignment 1
+**      1 2 3 4 5 
+**      1 2 3 4
+**      1 2 3
+**      1 2 
+**      1
+*/
+
+#include<stdio.h>
+
+int main()
+{
+    int i, j, num;
+
+    printf("Enter number of rows need to print = ");
+    scanf("%d", &num);
+
+    
+
+
+    return 0;
+}
+
+
+/*
+** Assignment 2
+**      6 5 4 3 2 1
+**      6 5 4 3 2
+**      6 5 4
+**      6 5 
+**      6
+*/
