eae6d91e617347e9c4cdb5a11c0de0f45bfa34e6
champ06 <rakuram.e96@gmail.com>
2021-09-04 23:31:28 +0530

    Inverted Number Patterns

--- Section 7 - Control Flow/Printing_Patterns/Printing_Patterns_5/040921_CLang_Printing_Patterns_5.c
+++ Section 7 - Control Flow/Printing_Patterns/Printing_Patterns_5/040921_CLang_Printing_Patterns_5.c
@@ -17,8 +17,15 @@ int main()
     printf("Enter number of rows need to print = ");
     scanf("%d", &num);
 
-    
-
+    for (i = num; i >= 1; --i)
+    {
+        // printf("%d", i);
+        for (j = 1; j <= i; ++j)
+        {
+            printf("%d ", j);
+        }
+        printf("\n");
+    }
 
     return 0;
 }
@@ -31,4 +38,28 @@ int main()
 **      6 5 4
 **      6 5 
 **      6
+*/
+
+/*
+#include<stdio.h>
+
+int main()
+{
+    int i, j, num;
+
+    printf("Enter number of rows need to print = ");
+    scanf("%d", &num);
+
+    for (i = 1; i <= num; ++i)
+    {
+        // printf("%d", i);
+        for (j = num; j >= i; --j)
+        {
+            printf("%d ", j);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
 */
