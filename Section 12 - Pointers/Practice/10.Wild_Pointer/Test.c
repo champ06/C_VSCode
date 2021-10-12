c0ffcd0a42f9bcbaca537bc362a83292e2fe6a85
Rakuram <rakuram.e96@gmail.com>
2021-10-12 19:56:54 +0530

    Testing

---
+++ Section 12 - Pointers/Practice/10.Wild_Pointer/Test.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+    /*
+    int a = 5;
+    int b = 2;
+    float c;
+    c = a/b;
+    printf("c = %f\n", c);
+    return 0;
+    */
+
+    unsigned int a = 0b00000101;
+    int y;
+
+    y = a << 1;
+    printf("%d\n", y);
+}
